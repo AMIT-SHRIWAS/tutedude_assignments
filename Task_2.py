a31feb107808ca0a40d78e833f248a2c04518bdd
AMIT-SHRIWAS <ashriwas702@gmil.com>
2025-11-14 23:21:25 +0530

    initial

---
+++ Task_2.py
@@ -0,0 +1,7 @@
+#  Personalized Greeting
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+ful_name = first_name + " " + last_name
+
+print('Hello, ' + ful_name+ '! welcome to the python program.')
